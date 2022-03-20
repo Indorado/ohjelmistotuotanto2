3d6d4a70e9aa818d61d7c28f8049a8a9516b0845
johan <johanna.pulli@outlook.com>
2022-03-20 12:09:12 +0200

    lisätty nappeja

--- src/components/OmatMatkat.js
+++ src/components/OmatMatkat.js
@@ -7,7 +7,7 @@ import Row from 'react-bootstrap/Row'
 import Image from 'react-bootstrap/Image'
 import Mainos from '../modules/Mainos';
 import './css/OmatMatkat.css';
-
+import Dropdown from 'react-bootstrap/Dropdown'
 
 
 //Johanna
@@ -19,38 +19,61 @@ const OmatMatkat = () => {
         <Row>
           <Col>
             <h1 className='header-text'>Omat matkat</h1>
-            <p className='text'>Jaa matkakertomuksesi kuvin ja tarinoin väritettynä muille käyttäjille!  Määritä matkakertomuksesi yksityisyys ja nauti tuoreina pysyvistä matkamuistoista!</p>
+            <p className='text'>Jaa matkakertomuksesi kuvin ja tarinoin väritettynä muille käyttäjille!
+              Määritä matkakertomuksesi yksityisyys ja nauti tuoreina pysyvistä matkamuistoista!</p>
             <Buttons />
           </Col>
           <Col><Image class="img-fluid" src="https://imgur.com/r5cDrZd.png"></Image></Col>
         </Row>
       </Container>
-
-
       <hr></hr>
-
-
       <Container>
         <div>
-          <h2>Selaa omia matkoja</h2>
-          <p className='text'>Selaa itse lisäämiäsi matkakohteita ja uppoudu kuvin ja tarinoin väritettyihin matkakertomuksiisi!</p>
           <Row>
             <Col>
-              {/*</DropDownList>*/}
-              <Buttons2 />
-              <p className='text'>*Tähän sisältöä*</p>
+              <h2>Selaa omia matkoja</h2>
+              <p className='text'>Selaa itse lisäämiäsi matkakohteita ja uppoudu kuvin ja tarinoin väritettyihin matkakertomuksiisi!</p>
+            </Col>
+
+            <Col>
+              {/*</DropDown>*/}
+              <Dropdown>
+                <p className='text'>Voit rajata hakua alla olevasta valikosta valitsemalla kohteen ja painamalla "Rajaa hakutulosta"-nappia.</p>
+                <Dropdown.Toggle variant="secondary" id="dropdown-basic">Valitse matkakohde</Dropdown.Toggle>
+                <button type="button" class="btn btn-outline-secondary" style={{ marginLeft: 7 }}>Rajaa hakutulosta</button>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Suomi</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">Bulgaria</Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">Espanja</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </Col>
           </Row>
           <Row>
             {/*kuvat tähän*/}
           </Row>
-
         </div>
       </Container>
 
-
+      {/*Mainos*/}
       <div>
-        <Mainos />
+        <hr></hr>
+        <Container>
+          <Row>
+            <Col>
+              <h2>Tallenna omia matkakertomuksia</h2>
+              <p className='text'>Määritä matkakertomuksesi yksityisyys ja väritä omia matkakertomuksia
+               kuvin ja tarinoin! Voit myös uppoutua muiden käyttäjien matkakertomuksiin ja
+                etsiä inspiraatioita seuraavalle matkallesi!</p>
+                <Buttons2/>
+            </Col>
+            <Col><img class="img-fluid" src="https://i.imgur.com/US0C0Lc.png" alt=""></img></Col>
+          </Row>
+          
+        </Container>
+
+        <hr></hr>
       </div>
 
       <div className='sign-up'>
@@ -71,22 +94,21 @@ const OmatMatkat = () => {
 const Buttons = () => {
   return (
     <div className="buttons">
-      <Button className='btn-in' variant="secondary">Tallenna matkasi</Button>
-      <Button variant='light' style={{ color: "grey", backgroundColor: 'lightgray' }}>Omat tiedot</Button>
+      <Button className='btn-in' variant="secondary">Lisää uusi matka</Button>
+      <button type="button" class="btn btn-outline-secondary" style={{ marginLeft: 2 }}>Omat tiedot</button>
     </div>
   )
 }
 
 const Buttons2 = () => {
   return (
-    <div className="buttonsOmatMatkat" style={{ float: 'right' }}>
-      <Button class="testi" variant="secondary" >Rajaa hakutulosta</Button>
-      <br></br>
-      <Button variant='light' style={{ color: "grey", backgroundColor: 'lightgray' }}>Muokkaa matkaa</Button>
-      <br></br>
-      <Button className="btn-in" variant='secondary'>Lisää matka</Button>
-      <br></br>
-      <Button variant='light' style={{ color: "grey", backgroundColor: 'lightgray' }}>Poista matka</Button>
+    <div className="btnMainos" >
+      <p className="text">Tästä pääset muokkaamaan, lisäämään ja poistamaan luomiasia matkakohteita.</p>
+      <button type="button" class="btn btn-outline-secondary">Muokkaa</button>
+      
+      <button type="button" class="btn btn-outline-secondary" style={{ marginLeft: 5 }}>Lisää</button>
+     
+      <button type="button" class="btn btn-outline-secondary" style={{ marginLeft: 5 }}>Poista</button>
     </div>
   )
 }
