ffa571314f5b914503807b83aac71a9e100904fe
OskarToivanen <samueltoiva@gmail.com>
2022-04-11 21:13:52 +0300

    sivun päivitys

--- src/components/create.js
+++ src/components/create.js
@@ -26,21 +26,21 @@ export default function Create() {
     e.preventDefault();
 
     // When a post request is sent to the create url, we'll add a new record to the database.
-    const newPerson = { ...form };
+    const newStory = { ...form };
 
     await fetch("http://localhost:5000/record/add", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(newPerson),
+      body: JSON.stringify(newStory),
     }).catch((error) => {
       window.alert(error);
       return;
     });
 
     setForm({ kohde: "", paikka: "", maa: "", kuvaus: "" });
-    navigate("/");
+    window.location.reload();
   }
 
   // This following section will display the form that takes the input from the user.
