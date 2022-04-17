f24867c4f4cc628c272b0fb077f2211826481ce7
OskarToivanen <samueltoiva@gmail.com>
2022-04-17 18:52:39 +0300

    pieniä muutoksia

--- src/components/Koti.js
+++ src/components/Koti.js
@@ -148,7 +148,7 @@ const LoginFormSignIn = ({ onSubmit }) => {
   };
 
   return (
-    <Form onSubmit={loginSubmit2}>
+    <Form onSubmit={loginSubmit2} action="/login">
 
            <Form.Group controlId="formBasic" className="formBasic">
         <Form.Control
@@ -261,7 +261,7 @@ const LoginFormSignUp = ({ onSubmit }) => {
 
   return (
     /*<Form onSubmit={onSubmit}*/
-    <Form onSubmit={loginSubmit}>
+    <Form onSubmit={loginSubmit} action="/register">
 
       <Form.Group controlId="formBasic" className="formBasic">
         <Form.Control
